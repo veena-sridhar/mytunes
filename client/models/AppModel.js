4186716a9a401ad87b8f04c995762701f07b2ec8
veena-sridhar <v.sridhar91@gmail.com>
2016-05-04 19:02:03 -0700

    Pass specs for App

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -5,6 +5,7 @@ var AppModel = Backbone.Model.extend({
     this.set('currentSong', new SongModel());
     this.set('songQueue', new SongQueue());
 
+
     /* Note that 'this' is passed as the third argument. That third argument is
     the context. The 'play' handler will always be bound to that context we pass in.
     In this example, we're binding it to the App. This is helpful because otherwise
@@ -16,6 +17,13 @@ var AppModel = Backbone.Model.extend({
     params.library.on('play', function(song) {
       this.set('currentSong', song);
     }, this);
+
+    params.library.on('enqueue', function (song) {
+      var songs = this.get('songQueue');
+      songs.push(song);
+    }, this);
   }
+
+
 
 });
